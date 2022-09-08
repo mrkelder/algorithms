ffdba45dca45d3c42a7865a3f53d8428947b8b42
Vlad Ageev <vageev06@gmail.com>
2022-09-08 11:46:49 +0300

    feat: linkedList

---
+++ LinkekLists/LinkedLists.js
@@ -0,0 +1,63 @@
+class LinkedList {
+  #list = null;
+
+  print() {
+    let currentLink = this.#list;
+    if (!currentLink) console.log("List is empty");
+    while (currentLink) {
+      console.log(currentLink.val);
+      currentLink = currentLink.next;
+    }
+
+    return this;
+  }
+
+  insert(val) {
+    const newLink = { val: val, next: null };
+    if (!this.#list) this.#list = newLink;
+    else {
+      let currentLink = this.#list;
+      while (currentLink.next !== null) currentLink = currentLink.next;
+      currentLink.next = newLink;
+    }
+
+    return this;
+  }
+
+  remove() {
+    if (!this.#list || !this.#list.next) this.#list = null;
+    else if (this.#list) {
+      let currentLink = this.#list;
+      let prevLink = null;
+      while (currentLink.next !== null) {
+        prevLink = currentLink;
+        currentLink = currentLink.next;
+      }
+
+      prevLink.next = null;
+    }
+
+    return this;
+  }
+}
+
+const ll = new LinkedList();
+
+ll.insert(1)
+  .insert(2)
+  .insert(3)
+  .insert(4)
+  .insert(5)
+  .print()
+  .remove()
+  .remove()
+  .remove()
+  .print()
+  .remove()
+  .print()
+  .remove()
+  .print()
+  .remove()
+  .remove()
+  .remove()
+  .print();
