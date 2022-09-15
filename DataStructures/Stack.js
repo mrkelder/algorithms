d57567cf094fca3406c2a744edd1a36f30fba6db
Vlad Ageev <vageev06@gmail.com>
2022-09-15 14:10:47 +0300

    feat: Stack

---
+++ DataStructures/Stack.js
@@ -0,0 +1,21 @@
+const { LinkedList } = require("./LinkedLists");
+
+// built-in kind of stack
+const stack1 = [];
+
+stack1.push(1);
+stack1.push(2);
+stack1.push(3);
+console.log(stack1);
+stack1.pop();
+console.log(stack1);
+
+// stack with linked lists
+
+const stack2 = new LinkedList();
+
+stack2.insert(1);
+stack2.insert(2);
+stack2.insert(3);
+stack2.print();
+stack2.remove();
